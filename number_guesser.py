45e95317db6e8136da8939c651d52cb1d2d81803
Martin <martin.razak@gmail.com>
2026-05-08 21:12:18 +0200

    Add project files

---
+++ number_guesser.py
@@ -0,0 +1,53 @@
+import random
+
+def show_instructions():
+    print()
+    print('Guess a number between 1 and 50.')
+    print('Try to  win in as few attempts as possible.')
+    print()
+
+def play_game():
+    secret_number = random.randint(1, 50)
+
+    guess = None
+    attempts = 0
+
+    while guess != secret_number:
+        try:
+            guess = int(input('Guess a number between 1 and 50: '))
+            attempts += 1
+        except ValueError:
+            print('Please enter a number brochacho')
+            continue
+        if guess < secret_number:
+            print('Guess higher')
+        elif guess > secret_number:
+            print('Guess lower')
+    print(f"You win, it took you {attempts} attempts")
+
+def menu():
+    while True:
+        choice = input('Press 1 to play, 2 to show instructions, 3 to exit: > ')
+        if choice.isdigit():
+            choice = int(choice)
+            if choice in(1, 2, 3):
+                return choice
+            else:
+                print("Please enter 1, 2 or 3 brochacho")
+        else:
+            print("Please enter a number brochacho")
+
+
+print('Welcome to the number guessing game')
+while True:
+    choice = menu()
+    if choice == 1:
+        play_game()
+    elif choice == 2:
+        show_instructions()
+    elif choice == 3:
+        break
+
+
+
+
